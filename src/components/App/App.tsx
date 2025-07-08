3466314f49d225966a4fd1fba373ef5274897530
Oksanamsangi <162930811+Oksanamsangi@users.noreply.github.com>
2025-07-08 00:05:15 -0600

    Initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,48 @@
+import CafeInfo from "../CafeInfo/CafeInfo";
+import css from "./App.module.css";
+import { useState } from "react";
+import { type VoteType, type Votes } from "../../types/votes";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notifications from "../Notifications/Notifications";
+
+function App() {
+  const [votes, setVotes] = useState<Votes>({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const resetVotes = () => {
+    setVotes({
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    });
+  };
+  const handleVote = (type: VoteType) => {
+    setVotes((prevVotes) => ({
+      ...prevVotes,
+      [type]: prevVotes[type] + 1,
+    }));
+    
+  };
+
+  const totalVotes = votes.good + votes.neutral + votes.bad
+  const positiveRate = (totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0);
+
+  return (
+    
+      <div className={css.app}>
+        <CafeInfo />
+        <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={totalVotes > 0} />
+        
+        {totalVotes > 0 ? (<VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate} />) : ( <Notifications />)} 
+      </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/VoteOptions/VoteOptions.tsx
@@ -0,0 +1,29 @@
+import { type VoteType } from "../../types/votes";
+import css from "./VoteOptions.module.css";
+
+interface Props {
+  onVote: (type: VoteType) => void;
+  onReset: () => void;
+  canReset: boolean;
+}
+
+export default function VoteOptions({ onVote, onReset, canReset }: Props) {
+  return (
+    <div className={css.container}>
+      <button onClick={() => onVote("good")} className={css.button}>
+        Good
+      </button>
+      <button onClick={() => onVote("neutral")} className={css.button}>
+        Neutral
+      </button>
+      <button onClick={() => onVote("bad")} className={css.button}>
+        Bad
+      </button>
+      {canReset && (
+        <button onClick={onReset} className={`${css.button} ${css.reset}`}>
+          Reset
+        </button>
+      )}
+    </div>
+  );
+}
